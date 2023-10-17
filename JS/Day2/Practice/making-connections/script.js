2abc8cc0c96ea63dce5ced169a2605790ad550cc
yahyaouirim <rimyahyaoui01@gmail.com>
2023-10-17 02:42:14 +0100

    making connection practice assignement

---
+++ JS/Day2/Practice/making-connections/script.js
@@ -0,0 +1,32 @@
+console.log("page loaded...");
+function editProfile(){
+    if(document.getElementById("jane").innerText=="Jane Doe"){
+        document.getElementById("jane").innerText="Yahyaoui Rim";
+
+    }
+    else{
+        document.getElementById("jane").innerText="Jane Doe";
+
+    }
+
+}
+var z=document.querySelector("#rnum");
+
+function removeUser(element,el1,el2){
+    element.remove();
+    el1.remove();
+    el2.remove();
+    z.innerText--;
+    console.log(z);
+
+}
+
+var y=500;
+
+function addConector()
+{  
+      
+     console.log(y);
+     y++;
+     document.getElementById("five").innerText=y;
+}
